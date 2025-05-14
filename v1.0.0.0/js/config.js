80c3e8d43581c2032184760fb452479e363e04e0
sjkim <sjkim@raoncorp.com>
2025-05-14 15:12:02 +0900

    버전 context path

--- v1.0.0.0/js/config.js
+++ v1.0.0.0/js/config.js
@@ -1,4 +1,4 @@
-const basePath = window.location.origin + "/did-demo-app/";
+const basePath = window.location.origin + "/did-demo-app/v1.0.0.0/";
 
 function setPage(page) {
     window.location.href = basePath + page;
