508f58d0b15e72a81d2b455caf7880cddd92ac0d
sjkim <sjkim@raoncorp.com>
2025-05-14 15:53:23 +0900

    sss

--- v2.0.0.0/js/version.js
+++ v2.0.0.0/js/version.js
@@ -4,19 +4,11 @@
     window.addEventListener("DOMContentLoaded", function () {
         const versionDiv = document.createElement("div");
         versionDiv.textContent = version;
+        versionDiv.id = "version-bar";
 
-        versionDiv.style.backgroundColor = "#000";
-        versionDiv.style.color = "#fff";
-        versionDiv.style.textAlign = "center";
-        versionDiv.style.padding = "6px 0";
-        versionDiv.style.fontSize = "12px";
-        versionDiv.style.width = "375px";
-        versionDiv.style.borderBottomLeftRadius = "20px";
-        versionDiv.style.borderBottomRightRadius = "20px";
-
-        const mobileContainer = document.querySelector(".mobile-container");
-        if (mobileContainer && mobileContainer.parentElement) {
-            mobileContainer.parentElement.insertBefore(versionDiv, mobileContainer.nextSibling);
+        const wrapper = document.querySelector(".container-wrapper");
+        if (wrapper) {
+            wrapper.appendChild(versionDiv);
         }
     });
 })();
