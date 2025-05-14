d4d90590c9fdc8ab82f549172593706cf032713d
sjkim <sjkim@raoncorp.com>
2025-05-14 13:53:58 +0900

    버그 수정

--- js/config.js
+++ js/config.js
@@ -3,3 +3,7 @@ const basePath = window.location.origin + "/did-demo-app/";
 function setPage(page) {
     window.location.href = basePath + page;
 }
+
+function getBasePath() {
+    return basePath;
+}
